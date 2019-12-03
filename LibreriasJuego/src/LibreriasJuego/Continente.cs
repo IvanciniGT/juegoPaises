6079e37a78f5e62b811ce8243bc6067235991a6e
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2019-12-03 14:03:48 +0100

    Listo por hoy

---
+++ LibreriasJuego/src/LibreriasJuego/Continente.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LibreriasJuego
+{
+    public class Continente: IContinente
+    {
+        internal Continente(string nombre) {
+            this.nombre = nombre;
+            this.paises = new List<IPais>();
+            this.misPaises = new Dictionary<string, IPais>();
+
+        }
+        private Dictionary<string,IPais> misPaises { get; }
+
+        public string nombre { get; }
+
+        public List<IPais> paises { get=>{
+                return X;
+            } 
+        }
+
+        public IPais getPais(string nombrePais) {
+            return this.misPaises[nombrePais];
+        }
+
+        internal void asignarPais(IPais nuevoPais) {
+            this.misPaises.Add(nuevoPais.nombre, nuevoPais);
+        }
+
+
+    }
+}
