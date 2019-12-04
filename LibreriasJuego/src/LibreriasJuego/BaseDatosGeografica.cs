99e5d8b7075122b699dad266cb40a330f8950113
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2019-12-04 11:25:23 +0100

    Todo para arriba

---
+++ LibreriasJuego/src/LibreriasJuego/BaseDatosGeografica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LibreriasJuego
+{
+    public class BaseDatosGeografica: IBaseDatosGeografica
+    {
+        internal BaseDatosGeografica() {
+            this.paises = new Dictionary<string, IPais>();
+            this.continentes = new Dictionary<string, IContinente>();
+        }
+
+        private Dictionary<string, IPais> paises;
+        private Dictionary<string, IContinente> continentes;
+
+        public IPais getPais(string nombrePais) {
+            return this.paises[nombrePais];
+        }
+
+        public IContinente getContinente(string nombreContinente) {
+            return this.continentes[nombreContinente];
+        }
+
+        public IList<IContinente> getContinentes() {
+            return new List<IContinente>(this.continentes.Values).AsReadOnly();
+        }
+        
+    }
+}
